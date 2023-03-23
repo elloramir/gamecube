0d2002b534f93967558731998e1f5a6828e9b312
elloramir <cripboygit@gmail.com>
2023-03-23 17:42:29 -0300

    window and opengl helpers

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 ### GameCube
 Don't took it seriously, just me playing around OpenGL and Golang.
-The game world are composeda by chunks and chunks are small wrapper of voxels, that can be uploaded/unloaded
+The game world are composed by chunks and chunks are an wrapper of voxels, that can be uploaded/unloaded
 any time to GPU. It has some optimizations but nothing that awe.
 
 ### Table of contents
---
+++ main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"fmt"
+	_"embed"
+	"github.com/go-gl/glfw/v3.3/glfw"
+	"github.com/go-gl/gl/v3.3-core/gl"
+)
+
+var (
+	screenWidth = 800
+	screenHeight = 600
+)
+
+//go:embed res/game.vert
+var vertexSource string
+//go:embed res/game.frag
+var fragmentSource string
+
+func main() {
+	err := glfw.Init()
+	if err != nil {
+		panic(err)
+	}
+	defer glfw.Terminate()
+
+	// create window
+	glfw.WindowHint(glfw.Resizable, glfw.True)
+	glfw.WindowHint(glfw.ContextVersionMajor, 3)
+	glfw.WindowHint(glfw.ContextVersionMinor, 3)
+	glfw.WindowHint(glfw.OpenGLProfile, glfw.OpenGLCoreProfile)
+
+	window, err := glfw.CreateWindow(screenWidth, screenHeight, "", nil, nil)
+	if err != nil {
+		panic(err)
+	}
+
+	window.MakeContextCurrent()
+	glfw.SwapInterval(1)
+
+	// load opengl
+	err = gl.Init()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("welcome adventurer, to 'gamecube'")
+	fmt.Println(gl.GoStr(gl.GetString(gl.VERSION)))
+
+	for !window.ShouldClose() {
+
+		window.SwapBuffers()
+		glfw.PollEvents()
+	}
+}
