5cdc5f412f3c077d96657c62524ab087cc15b8f1
elloramir <cripboygit@gmail.com>
2023-04-23 22:35:09 -0300

    right face orders

--- main.go
+++ main.go
@@ -1,5 +1,84 @@
+// Copyright (c) 2023 Ellora.
+// Use of this source code is governed by MIT
+// license that can be found in the LICENSE file.
+
 package main
 
+import (
+	"fmt"
+	"log"
+	"runtime"
+
+	"github.com/elloramir/gamecube/game"
+	"github.com/elloramir/gamecube/gfx"
+	"github.com/elloramir/gamecube/world"
+	"github.com/go-gl/gl/v3.3-core/gl"
+	"github.com/go-gl/glfw/v3.3/glfw"
+)
+
+const (
+	windowWidth  = 800
+	windowHeight = 600
+)
+
+func init() {
+	// This is needed to arrange that main() runs on main thread.
+	// See GLFW (go-gl) documentation for functions that are only allowed to
+	// be called from the main thread.
+	runtime.LockOSThread()
+}
+
 func main() {
-	print("hello sailor\n")
+	if err := glfw.Init(); err != nil {
+		log.Fatalln("failed to initialize glfw:", err)
+	}
+	defer glfw.Terminate()
+
+	glfw.WindowHint(glfw.Resizable, glfw.True)
+	glfw.WindowHint(glfw.ContextVersionMajor, 3)
+	glfw.WindowHint(glfw.ContextVersionMinor, 3)
+	glfw.WindowHint(glfw.OpenGLProfile, glfw.OpenGLCoreProfile)
+	window, err := glfw.CreateWindow(windowWidth, windowHeight, "Cube", nil, nil)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	window.MakeContextCurrent()
+
+	// Initialize Glow
+	if err := gl.Init(); err != nil {
+		log.Fatalln("failed to initialize glow:", err)
+	}
+
+	version := gl.GoStr(gl.GetString(gl.VERSION))
+	fmt.Println("OpenGL version", version)
+
+	// Configure GL settings
+	gl.Enable(gl.DEPTH_TEST)
+	gl.Enable(gl.BLEND)
+	gl.Enable(gl.CULL_FACE)
+	gl.CullFace(gl.BACK)
+	gl.DepthFunc(gl.LESS)
+	gl.BlendFunc(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA)
+	gl.ClearColor(0.1, 0.2, 0.3, 1.0)
+
+	// Playground
+	chunk := world.NewChunk(0, 0)
+	camera := game.NewCamera()
+
+	program, _ := gfx.LoadShader("shaders/voxel.vert", "shaders/voxel.frag")
+
+	for !window.ShouldClose() {
+		// Render
+		gl.Viewport(0, 0, windowWidth, windowHeight)
+		gl.Clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
+
+		camera.SendUniforms(program)
+		gl.UseProgram(program)
+		chunk.Terrain.Render()
+
+		// Maintenance
+		camera.Update()
+		window.SwapBuffers()
+		glfw.PollEvents()
+	}
 }
--- world/block.go
+++ world/block.go
@@ -4,12 +4,15 @@
 
 package world
 
+import "github.com/elloramir/gamecube/gfx"
+
 type BlockKind uint8
 
 const (
-	BlockAir BlockKind = iota
+	BlockEmpty BlockKind = iota
 	BlockVoid
 	BlockGrass
+	BlockWater
 )
 
 type QuadSide uint32
@@ -23,50 +26,50 @@ const (
 	SideBottom
 )
 
-func generateQuad(Mesh *mesh, side QuadSide, i, j, k int32) {
-	xp := float32( 0.5 + x);
-	xn := float32(-0.5 + x);
-	yp := float32( 0.5 + y);
-	yn := float32(-0.5 + y);
-	zp := float32( 0.5 + z);
-	zn := float32(-0.5 + z);
+func generateQuad(mesh *gfx.Mesh, side QuadSide, i, j, k int32) {
+	xp := 0.5 + float32(i)
+	xn := -0.5 + float32(i)
+	yp := 0.5 + float32(j)
+	yn := -0.5 + float32(j)
+	zp := 0.5 + float32(k)
+	zn := -0.5 + float32(k)
 
 	switch side {
-		case SideNorth:
-			mesh.Vertex(xN, yP, zN, 0, 0, -1, 1, 1)
-			mesh.Vertex(xP, yN, zN, 0, 0, -1, 0, 0)
-			mesh.Vertex(xN, yN, zN, 0, 0, -1, 1, 0)
-			mesh.Vertex(xP, yP, zN, 0, 0, -1, 0, 1)
-			mesh.Quad(0, 1, 2, 0, 3, 1)
-		case SideSouth:
-			mesh.Vertex(xN, yP, zP, 0, 0, 1, 0, 1)
-			mesh.Vertex(xN, yN, zP, 0, 0, 1, 0, 0)
-			mesh.Vertex(xP, yN, zP, 0, 0, 1, 1, 0)
-			mesh.Vertex(xP, yP, zP, 0, 0, 1, 1, 1)
-			mesh.Quad(0, 1, 2, 0, 2, 3)
-		case SideEast:
-			mesh.Vertex(xP, yP, zN, 1, 0, 0, 1, 1)
-			mesh.Vertex(xP, yN, zP, 1, 0, 0, 0, 0)
-			mesh.Vertex(xP, yN, zN, 1, 0, 0, 1, 0)
-			mesh.Vertex(xP, yP, zP, 1, 0, 0, 0, 1)
-			mesh.Quad(0, 1, 2, 3, 1, 0)
-		case SideWest:
-			mesh.Vertex(xN, yP, zN, -1, 0, 0, 0, 1)
-			mesh.Vertex(xN, yN, zN, -1, 0, 0, 0, 0)
-			mesh.Vertex(xN, yN, zP, -1, 0, 0, 1, 0)
-			mesh.Vertex(xN, yP, zP, -1, 0, 0, 1, 1)
-			mesh.Quad(0, 1, 2, 3, 0, 2)
-		case SideTop:
-			mesh.Vertex(xN, yP, zP, 0, 1, 0, 0, 0)
-			mesh.Vertex(xP, yP, zP, 0, 1, 0, 1, 0)
-			mesh.Vertex(xN, yP, zN, 0, 1, 0, 0, 1)
-			mesh.Vertex(xP, yP, zN, 0, 1, 0, 1, 1)
-			mesh.Quad(0, 1, 2, 2, 1, 3)
-		case SideBottom:
-			mesh.Vertex(xN, yN, zP, 0, -1, 0, 0, 1)
-			mesh.Vertex(xN, yN, zN, 0, -1, 0, 0, 0)
-			mesh.Vertex(xP, yN, zP, 0, -1, 0, 1, 1)
-			mesh.Vertex(xP, yN, zN, 0, -1, 0, 1, 0)
-			mesh.Quad(0, 1, 2, 1, 3, 2)
+	case SideNorth:
+		mesh.Vertex(xn, yp, zn, 0, 0, -1, 1, 1)
+		mesh.Vertex(xp, yn, zn, 0, 0, -1, 0, 0)
+		mesh.Vertex(xn, yn, zn, 0, 0, -1, 1, 0)
+		mesh.Vertex(xp, yp, zn, 0, 0, -1, 0, 1)
+		mesh.Quad(0, 1, 2, 0, 3, 1)
+	case SideSouth:
+		mesh.Vertex(xn, yp, zp, 0, 0, 1, 0, 1)
+		mesh.Vertex(xn, yn, zp, 0, 0, 1, 0, 0)
+		mesh.Vertex(xp, yn, zp, 0, 0, 1, 1, 0)
+		mesh.Vertex(xp, yp, zp, 0, 0, 1, 1, 1)
+		mesh.Quad(0, 1, 2, 0, 2, 3)
+	case SideEast:
+		mesh.Vertex(xp, yp, zn, 1, 0, 0, 1, 1)
+		mesh.Vertex(xp, yn, zp, 1, 0, 0, 0, 0)
+		mesh.Vertex(xp, yn, zn, 1, 0, 0, 1, 0)
+		mesh.Vertex(xp, yp, zp, 1, 0, 0, 0, 1)
+		mesh.Quad(0, 1, 2, 3, 1, 0)
+	case SideWest:
+		mesh.Vertex(xn, yp, zn, -1, 0, 0, 0, 1)
+		mesh.Vertex(xn, yn, zn, -1, 0, 0, 0, 0)
+		mesh.Vertex(xn, yn, zp, -1, 0, 0, 1, 0)
+		mesh.Vertex(xn, yp, zp, -1, 0, 0, 1, 1)
+		mesh.Quad(0, 1, 2, 3, 0, 2)
+	case SideTop:
+		mesh.Vertex(xn, yp, zp, 0, 1, 0, 0, 0)
+		mesh.Vertex(xp, yp, zp, 0, 1, 0, 1, 0)
+		mesh.Vertex(xn, yp, zn, 0, 1, 0, 0, 1)
+		mesh.Vertex(xp, yp, zn, 0, 1, 0, 1, 1)
+		mesh.Quad(0, 1, 2, 2, 1, 3)
+	case SideBottom:
+		mesh.Vertex(xn, yn, zp, 0, -1, 0, 0, 1)
+		mesh.Vertex(xn, yn, zn, 0, -1, 0, 0, 0)
+		mesh.Vertex(xp, yn, zp, 0, -1, 0, 1, 1)
+		mesh.Vertex(xp, yn, zn, 0, -1, 0, 1, 0)
+		mesh.Quad(0, 1, 2, 1, 3, 2)
 	}
 }
